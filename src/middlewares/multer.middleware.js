0e2dba7e890cb1653023696d7aa7cbf12086cb57
Nikhil Dhamgay <nikhildhamgay200424@gmail.com>
2026-06-25 22:03:06 +0530

    cloudinary

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,13 +1,48 @@
+// import multer from "multer";
+// import path from "path";
+
+// const storage = multer.diskStorage({
+
+//     destination: function(req, file, cb){
+//         cb(null, "public/temp");
+//     },
+
+//     filename: function(req, file, cb){
+//         cb(
+//             null,
+//             `${Date.now()}-${Math.random()}${path.extname(file.originalname)}`
+//         );
+//     }
+// });
+
+// const upload = multer({
+//     storage
+// });
+
+// export default upload;
+
 import multer from "multer";
 import path from "path";
+import fs from "fs";
+
+const uploadDir = path.join(
+    process.cwd(),
+    "public",
+    "temp"
+);
+
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, {
+        recursive: true
+    });
+}
 
 const storage = multer.diskStorage({
-
-    destination: function(req, file, cb){
-        cb(null, "public/temp");
+    destination: function (req, file, cb) {
+        cb(null, uploadDir);
     },
 
-    filename: function(req, file, cb){
+    filename: function (req, file, cb) {
         cb(
             null,
             `${Date.now()}-${Math.random()}${path.extname(file.originalname)}`
@@ -15,8 +50,4 @@ const storage = multer.diskStorage({
     }
 });
 
-const upload = multer({
-    storage
-});
-
-export default upload;
+export default multer({ storage });
